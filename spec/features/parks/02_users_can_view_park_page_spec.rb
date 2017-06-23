bf1b19f6f89ee7e1ad289b6be70094e2dd840893
David Neto <davidneto92@gmail.com>
2017-06-23 18:58:06 -0400

    Park tests corrected

--- spec/features/parks/02_users_can_view_park_page_spec.rb
+++ spec/features/parks/02_users_can_view_park_page_spec.rb
@@ -10,7 +10,8 @@ feature "users view a page for each park" do
 
     expect(page).to have_content(park_01.name)
     expect(page).to have_content(park_01.state)
-    expect(page).to have_content("Type: #{park_01.park_type}")
+    expect(page).to have_content("Designation: #{park_01.park_type}")
+    expect(page).to have_content("Reachable by car: Yes")
     expect(page).to have_link("Directions to #{park_01.name}")
   end
 
@@ -20,6 +21,18 @@ feature "users view a page for each park" do
 
     click_link "Back to all parks"
     expect(page).to have_content("All National Parks")
+  end
+
+  scenario "correct lisiting if park cannot be driven to" do
+    park_01 = FactoryGirl.create(:park, drivable: false)
+
+    visit "/parks/#{park_01.id}"
+
+    expect(page).to have_content(park_01.name)
+    expect(page).to have_content(park_01.state)
+    expect(page).to have_content("Designation: #{park_01.park_type}")
+    expect(page).to have_content("Reachable by car: No")
+    expect(page).to_not have_link("Directions to #{park_01.name}")
   end
 
 
