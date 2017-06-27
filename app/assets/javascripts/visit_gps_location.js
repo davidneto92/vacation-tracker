e5655dd05dc4382b18b687962fb1eefeefd7329d
David Neto <davidneto92@gmail.com>
2017-06-27 14:02:30 -0400

    Visit GPS button working

--- app/assets/javascripts/visit_gps_location.js
+++ app/assets/javascripts/visit_gps_location.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     gpsButton.addEventListener("click", function(){
       event.preventDefault();
       userCoordinates();
+      // gpsButton.disabled = true;
     });
   }
 });
@@ -18,7 +19,6 @@ function userCoordinates() {
         lng: position.coords.longitude
       };
       findParks(geolocation);
-
     });
   }
 }
@@ -34,15 +34,43 @@ function findParks(coordinates) {
     url: '/api/v1/parks',
     success: function success(parks) {
       $.each(parks, function (x, park) {
-        if (between(coordinates.lat, park.latitude - 0.4, park.latitude + 0.4) && between(coordinates.lng, park.longitude - 0.4, park.longitude + 0.4) ) {
+        // searches a 25 mile area around user's location
+        if (between(coordinates.lat, park.latitude - 0.45, park.latitude + 0.45) && between(coordinates.lng, park.longitude - 0.45, park.longitude + 0.45) ) {
           foundParks.push(park);
         }
       });
-      debugger;
-      // TODO:
-      // send list to method that can present the parks
-      // list should be presented a list of buttons
-      // clicking a button will close the window and change the select box to the corresponding choice
+      displayFoundParks(foundParks);
+      tieButtonsToList(foundParks);
     }
   });
+}
+
+function displayFoundParks(foundParks) {
+  resultsDisplay = document.getElementById('foundParksDiv');
+  resultsDisplay.innerHTML = '';
+
+  buttonStartText = '<div class="row text-center"><div class="columns small-12"><span type="button" class="button expanded" ';
+  buttonEndText   = '</span></div></div>';
+
+  if (foundParks.length > 0) {
+    $.each(foundParks, function (x, park) {
+      resultsDisplay.innerHTML += (buttonStartText + 'id="result' + park.id + '">' + park.name + ' ' + park.park_type + buttonEndText);
+    });
+  } else {
+    resultsDisplay.innerHTML = 'No parks found near your location. Please try again';
+  }
+}
+
+function tieButtonsToList(foundParks) {
+  var dropdownList = document.getElementById('park-visit-list');
+
+  $.each(foundParks, function (x, park) {
+    button = document.getElementById('result' + park.id);
+    button.addEventListener("click", function(){
+      dropdownList.selectedIndex = park.id - 1;
+      resultsDisplay = document.getElementById('foundParksDiv');
+      resultsDisplay.innerHTML = '';
+    });
+  });
+
 }
